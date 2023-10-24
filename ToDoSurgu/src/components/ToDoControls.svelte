4545bb9a75c143c83f235be5c5cd6f0a855ac5c3
Stenina Anastasia <awstenina@gmail.com>
2023-10-24 20:46:31 +0500

    First commit

---
+++ ToDoSurgu/src/components/ToDoControls.svelte
@@ -0,0 +1,37 @@
+<script>
+    import{createEventDispatcher} from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    let inputValue='';
+    function onClick(){
+        if(inputValue.length){
+            dispatch('add', {text: inputValue});
+            inputValue='';
+        }
+    }
+</script>
+
+<div class="todo-controls">
+    <input class="todo-controls_input" type="text" bind:value={inputValue}/>
+    <button class="todo-controls_btn" on:click={onClick}>Add</button>
+    
+</div>
+<style>
+    .todo-controls{
+        display: grid;
+        grid-template-columns: 70% 30%;
+
+    }
+    .todo-controls_input{
+        border: none;
+        border-radius: 10px 0 0 10px;
+        background: rgb(255, 255, 255);
+        padding: 15px;
+    }
+    .todo-controls_btn{
+        border: none;
+        border-radius: 0 10px 10px 0;
+        background: rgb(255, 207, 96);
+        padding: 10px;
+    }
+</style>
---
+++ ToDoSurgu/src/components/ToDoItem.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let id;
+    export let text;
+    export let isDone;
+    import{createEventDispatcher} from 'svelte';
+
+    const dispatch=createEventDispatcher();
+</script>
+
+<div class="todo-item" class:todo-item_done={isDone}>
+    <input class="todo-item_check" type="checkbox" on:change={dispatch('change',{id})} checked={isDone}/>
+    <span class="todo-item_text">{text}</span>
+    <button class="todo-item_btn">Del</button>
+</div>
+
+<style>
+    .todo-item{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 15px;
+        background: rgb(250, 195, 195);
+        border-radius: 10px;
+    }
+    .todo-item_done{
+        background: rgb(166, 229, 166);
+    }
+    .todo-item_done .todo-item_text{
+        text-decoration: line-through;
+    }
+    .todo-item_check{
+
+    }
+    .todo-item_text{
+        flex-grow: 1;
+    }
+    .todo-item_btn{
+        border: none;
+        background-color: rgb(191, 118, 92);
+        border-radius: 10px;
+        padding: 10px;
+        min-width: 100px;
+    }
+</style>
